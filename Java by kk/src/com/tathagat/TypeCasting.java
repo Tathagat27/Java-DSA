51ecb51c6cd46f5449f59add577d56a9e18eeaf8
Tathagat27 <tat27302@gmail.com>
2022-07-19 17:00:50 +0530

    type casting notes link added

--- Java by kk/src/com/tathagat/TypeCasting.java
+++ Java by kk/src/com/tathagat/TypeCasting.java
@@ -1,13 +1,13 @@
 package com.tathagat;
 
-import java.util.Scanner;
 
 public class TypeCasting {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
 //        TypeCasting || automatically into bigger one
 //                    || forced by using (datatype)
         int num=(int)27.29;
         System.out.println(num);
     }
 }
+//https://github.com/kunal-kushwaha/DSA-Bootcamp-Java/blob/main/lectures/05-first-java-program/notes/type_conversion%26casting.txt
